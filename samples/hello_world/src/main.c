a5316c378a71e61a6d9e1cd3bb0b24ebd7ebc714
Afonso Oliveira <afonso.oliveira707@gmail.com>
2026-03-25 11:05:11 +0000

    boards: aifoundry: erbium: disable UART/PLIC for bare validation test
    Disable serial console, PLIC, and interrupts. Replace hello_world
    printf with a direct CSR write to the validation register so the
    sample can run on the emulator without UART.

--- samples/hello_world/src/main.c
+++ samples/hello_world/src/main.c
@@ -4,11 +4,16 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <stdio.h>
-
 int main(void)
 {
-	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+#ifdef CONFIG_SOC_ERBIUM_MINION
+	/* Signal end of test to cosim/VCS */
+	__asm__ volatile(
+		"fence\n"
+		"lui a7, 0x1FEED\n"
+		"csrw 0x8d0, a7\n"
+	);
+#endif
 
 	return 0;
 }
